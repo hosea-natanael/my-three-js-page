c1a565b184b69011e72f3ca73dfb6a99be715a93
ochamug <hosea.natanael22@gmail.com>
2020-12-31 14:34:40 +0700

    fix path

--- post-processing.js
+++ post-processing.js
@@ -1,7 +1,7 @@
-import * as THREE from "./node_modules/three/build/three.module.js";
-import {EffectComposer} from "./node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
-import {RenderPass} from "./node_modules/three/examples/jsm/postprocessing/RenderPass.js";
-import {UnrealBloomPass} from "./node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
+import * as THREE from "node_modules/three/build/three.module.js";
+import {EffectComposer} from "node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
+import {RenderPass} from "node_modules/three/examples/jsm/postprocessing/RenderPass.js";
+import {UnrealBloomPass} from "node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
 
 
 let postProcessing = (renderer, scene, camera)=>{
